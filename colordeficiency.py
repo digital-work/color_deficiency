8b4eec1e29a6a1cf4838cbea7af0457a4d965cde
joschua <thomas.simonliedtke@gmail.com>
2014-01-31 12:25:46 +0100

    Included original Vienot simulation.

--- colordeficiency.py
+++ colordeficiency.py
@@ -2,4 +2,225 @@
 ### File created by Joschua Simon-Liedtke on 22nd of January 2014
 ###
 
-print 'Das ist eine neue Datei mit Simulationen und Daltonisierungen'
+from PIL import Image
+import numpy
+import colour
+
+simulation_types = ["vienot", "vienot adjusted"]
+coldef_types = ["d","p","t"]
+img_in = Image.open("test.jpg")
+
+def makeLMSDeficientMatrix(rgb2lms, coldef_type):
+    """
+    Make color deficiency reduction matrices based on the on the algorithms proposed by Brettel and Vienot
+    """
+    
+    l = numpy.array([1,0,0])
+    m = numpy.array([0,1,0])
+    s = numpy.array([0,0,1])
+    
+    # alle rgb[0][...] - L, alle rgb[1][...] - M, alle rgb[2][...] - S
+    # alle rgb[...][0] - R, alle rgb[...][1] - G, alle rgb[...][2] - B 
+    
+    if coldef_type == "p":
+        #Matrix for deuteranopes
+        alpha   = sum(rgb2lms[1][0:3]) * rgb2lms[2][2]- rgb2lms[1][2] * sum(rgb2lms[2][0:3])
+        beta    = sum(rgb2lms[2][0:3]) * rgb2lms[0][2]- rgb2lms[2][2] * sum(rgb2lms[0][0:3])
+        gamma   = sum(rgb2lms[0][0:3]) * rgb2lms[1][2]- rgb2lms[0][2] * sum(rgb2lms[1][0:3])
+        
+        l_p = (-1.)/( alpha ) * numpy.array([0,beta,gamma])
+        l = l_p 
+        
+        m = numpy.array([0,1,0])
+        s = numpy.array([0,0,1])
+        
+        
+    elif coldef_type == "d":
+        #Matrix for protanopes
+        alpha   = sum(rgb2lms[1][0:3]) * rgb2lms[2][2]- rgb2lms[1][2] * sum(rgb2lms[2][0:3])
+        beta    = sum(rgb2lms[2][0:3]) * rgb2lms[0][2]- rgb2lms[2][2] * sum(rgb2lms[0][0:3])
+        gamma   = sum(rgb2lms[0][0:3]) * rgb2lms[1][2]- rgb2lms[0][2] * sum(rgb2lms[1][0:3])
+        
+        l = numpy.array([1,0,0])
+        
+        m_d = (-1.)/( beta ) * numpy.array([alpha,0,gamma])
+        m = m_d
+        
+        s = numpy.array([0,0,1])
+        
+    elif coldef_type == "t":
+        #Matrix for tritanopes
+        alpha   = sum(rgb2lms[1][0:3]) * rgb2lms[2][0]- rgb2lms[1][0] * sum(rgb2lms[2][0:3])
+        beta    = sum(rgb2lms[2][0:3]) * rgb2lms[0][0]- rgb2lms[2][0] * sum(rgb2lms[0][0:3])
+        gamma   = sum(rgb2lms[0][0:3]) * rgb2lms[1][0]- rgb2lms[0][0] * sum(rgb2lms[1][0:3])
+        
+        l = numpy.array([1,0,0])
+        m = numpy.array([0,1,0])
+        
+        s_t = (-1.)/( gamma ) * numpy.array([alpha,beta,0])
+        s = s_t
+    else:
+        print "Error: unknown color deficiency chosen. Chose either p for protanopes, d for deuteranopes, or t for tritanopes."
+   
+    matrix = numpy.array([l,m,s])
+    
+    return matrix
+    
+#print makeLMSDeficientMatrix(rgb2lms_vienot,"d")   
+
+def simulation_vienot(img_in, coldef_type):
+    """
+    This is a colour deficiency simulation for deuteranopes and protanopes based on 'Computerized ...' by Francoise Vienot et al.
+    Input values:    img_in - original PIL image
+                     coldef_type - type of color deficiency. d for deuteranopia, p for protanopia
+    Output values:   img_out - simulated PIL image 
+    """
+    
+    img_in = img_in.convert('RGB')
+    img_array = (numpy.asarray(img_in, dtype=float)/255.)**2.2
+    m,n,dim = numpy.shape(img_array)
+    
+    #     #Compression such that palette is within monitor gamut
+    #     if coldef_type == "p":
+    #         #img_array_com = 0.992052*img_array + 0.003974
+    #         #img_array = img_array_com
+    #         pass
+    #     elif coldef_type == "d": 
+    #         #img_array_com = 0.957237*img_array + 0.0213814
+    #         #img_array = img_array_com
+    #         pass
+    if not (coldef_type == "p" or coldef_type == "d"):
+        print "Error: unknown color deficiency chosen. Chose either p for protanopes, or d for deuteranopes."
+        
+        return img_in
+    
+    
+    # Modified RGB space based on ITU-R BT.709 primaries - same as sRGB - and Judd-Vos colorimetric modification
+    rgb2xyz = numpy.array([[40.9568, 35.5041,17.9167],
+                           [21.3389, 70.6743, 7.9868],
+                           [ 1.86297,11.462, 91.2367]])
+    vienotRGBSpaceLinear = colour.space.TransformLinear(colour.space.xyz,numpy.linalg.inv(rgb2xyz))
+    vienotRGBOriginal_arr = colour.data.Data(vienotRGBSpaceLinear, img_array)
+    XYZOriginal_arr = colour.data.Data(colour.space.xyz,vienotRGBOriginal_arr.get(colour.space.xyz))
+        
+    # LMS space based on Smith and Pokorny
+    xyz2lms = numpy.array([[.15514, .54312, -.03286],
+                           [-.15514, .45684, .03286],
+                           [0, 0, .01608]])
+    lmsSpace = colour.space.TransformLinear(colour.space.xyz,xyz2lms) #.01608 .00801
+    lmsOriginal_arr = XYZOriginal_arr.get(lmsSpace)
+    
+    rgb2lms = numpy.dot(xyz2lms,rgb2xyz)
+    lms2lms_deficient = makeLMSDeficientMatrix(rgb2lms, coldef_type)
+    
+    # This is the actual simulation
+    lmsOriginal_vector = numpy.reshape(lmsOriginal_arr,(m*n,3))
+    lmsSimulated_vector = numpy.dot(lmsOriginal_vector, lms2lms_deficient.transpose())
+    lmsSimulated_arr = colour.data.Data(lmsSpace, numpy.reshape(lmsSimulated_vector, (m,n,3)))
+    
+    # We propose this gamut clipping instead for hte one proposed by vienot
+    rgbVienot_arr = lmsSimulated_arr.get(vienotRGBSpaceLinear)
+    rgbVienot_arr[rgbVienot_arr<0] = 0
+    rgbVienot_arr[rgbVienot_arr>1] = 1
+    
+    vienotRGBSimulated_arr = (rgbVienot_arr**(1/2.2))*255.
+    img_array = numpy.uint8(vienotRGBSimulated_arr)
+    
+    img_out = Image.fromarray(img_array)
+    
+    return img_out
+
+def simulation_vienot_adjusted(img_in, coldef_type):
+    """
+    This is a colour deficiency simulation for deuteranopes and protanopes based on 'Computerized ...' by Francoise Vienot et al.
+    Some variations have been made: instead of using the gamma correction proposed in the paper, we use the standard sRGB conversion from sRGB to XYZ and we used an adjusted XYZ to LMS matrix.
+    Input values:    img_in - original PIL image
+                     coldef_type - type of color deficiency. d for deuteranopia, p for protanopia
+    Output values:   img_out - simulated PIL image 
+    """
+    
+    img_in = img_in.convert('RGB')
+    img_array = numpy.asarray(img_in, dtype=float)/255.
+    m,n,dim = numpy.shape(img_array)
+    
+    if not (coldef_type == "p" or coldef_type == "d" or coldef_type == "t"):
+        print "Error: Unknown color deficiency chosen. Chose either p for protanopes, d for deuteranopes, or t for tritanopes."
+        
+        return img_in
+    
+    
+    # Modified RGB space based on ITU-R BT.709 primaries - same as sRGB - and Judd-Vos colorimetric modification
+    xyz2rgb = numpy.array([[ 3.2404542, -1.5371385, -0.4985314],
+                           [-0.9692660,  1.8760108,  0.0415560],
+                           [ 0.0556434, -0.2040259,  1.0572252]])
+    rgb2xyz = numpy.linalg.inv(xyz2rgb)
+    
+    
+    
+    #     numpy.array([[40.9568, 35.5041,17.9167],
+    #                            [21.3389, 70.6743, 7.9868],
+    #                            [ 1.86297,11.462, 91.2367]])
+    #vienotRGBSpaceLinear = colour.space.TransformLinear(colour.space.xyz,numpy.linalg.inv(rgb2xyz))
+    #vienotRGBOriginal_arr = colour.data.Data(vienotRGBSpaceLinear, img_array)
+    #XYZOriginal_arr = colour.data.Data(colour.space.xyz,vienotRGBOriginal_arr.get(colour.space.xyz))
+        
+    # LMS space based on Smith and Pokorny
+    xyz2lms = numpy.array([[.15514, .54312, -.03286],
+                           [-.15514, .45684, .03286],
+                           [0, 0, .00801]])
+    lmsSpace = colour.space.TransformLinear(colour.space.xyz,xyz2lms) #.01608 .00801
+    lmsOriginal_arr = XYZOriginal_arr.get(lmsSpace)
+    
+    rgb2lms = numpy.dot(xyz2lms,rgb2xyz)
+    lms2lms_deficient = makeLMSDeficientMatrix(rgb2lms, coldef_type)
+    
+    # This is the actual simulation
+    lmsOriginal_vector = numpy.reshape(lmsOriginal_arr,(m*n,3))
+    lmsSimulated_vector = numpy.dot(lmsOriginal_vector, lms2lms_deficient.transpose())
+    lmsSimulated_arr = colour.data.Data(lmsSpace, numpy.reshape(lmsSimulated_vector, (m,n,3)))
+    
+    # We propose this gamut clipping instead for hte one proposed by vienot
+    rgbVienot_arr = lmsSimulated_arr.get(vienotRGBSpaceLinear)
+    rgbVienot_arr[rgbVienot_arr<0] = 0
+    rgbVienot_arr[rgbVienot_arr>1] = 1
+    
+    vienotRGBSimulated_arr = (rgbVienot_arr**(1/2.2))*255.
+    img_array = numpy.uint8(vienotRGBSimulated_arr)
+    
+    img_out = Image.fromarray(img_array)
+    
+    return img_out
+
+def simulate(img_in, coldef_type, simulation_type):
+    """
+    Function to simulate color deficiency for vienot, vienot adjusted.
+    Input:  img_in -          Original PIL image
+            coldef_type -     Type of color deficiency. Either p for protanopes, d for deuteranopes, or t for tritanopes.
+            simulation_type - 
+    Output: img_out -         Simulated PIL image
+    """
+    img_out = img_in
+    
+    if simulation_type == "vienot":
+        img_out = simulation_vienot(img_in, coldef_type)
+    elif simulation_type == "vienot_adjusted":
+        img_out = simulation_vienot_adjusted(img_in, coldef_type)
+    else:
+        print 'Error: Simulation type does not exist. Choose either one of the following - "'+'" , "'.join(simulation_types)+'".'
+    return img_out
+
+simulate(img_in,"d","videnot").show()
+
+def test():
+    name = "test3"
+    
+    im = Image.open(name+".jpg")
+    #im.show()
+    
+    for coldef_type in coldef_types:
+        im_sim = simulation_vienot(im, coldef_type)
+        #im_sim.show()
+        im_sim.save(name+"-"+coldef_type+".jpg")
+        print coldef_type + " simulation done"    
+
+#test()
