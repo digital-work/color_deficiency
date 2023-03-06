6856e70594ba31c8f354e28937d1162659430924
Joschua Thomas Simon-Liedtke (win-10) <joschua@nr.no>
2023-03-06 14:58:35 +0100

    Created new main file.

---
+++ __main__.py
@@ -0,0 +1,23 @@
+'''
+  Created on 6. mar 2023.
+  
+  Last updated on 6. mars. 2023.
+  
+  @author. joschua
+  
+  This package is used to simulate and daltonize images and arrays.
+  
+
+'''
+
+
+import argparse
+
+def colordeficiency():
+   
+   parser = argparse.ArgumentParser("A command line script to simulate color deficiency or daltonize images.")
+ 
+   args = parser.parse_args()
+
+if __name__=='__main__':
+  colordeficiency()
