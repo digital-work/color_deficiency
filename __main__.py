14f1d00e1120a53825b1a53c8ecdbb65ae8fbf16
Joschua Thomas Simon-Liedtke (win-10) <joschua@nr.no>
2023-03-20 16:40:25 +0100

    Included option to choose simulation method.

--- __main__.py
+++ __main__.py
@@ -32,6 +32,14 @@ coldef_types = {
       't': 'tritan'
    }
 
+sim_methods = {
+      'v': "vienot", 
+      'va': 'vienot-adjusted', 
+      'k': 'kotera',
+      'b': 'brettel',
+      'd': 'dummy'
+   }
+
 def colordeficiency():
    
    parser = argparse.ArgumentParser("A command line script to simulate color deficiency or daltonize images.")
@@ -39,16 +47,21 @@ def colordeficiency():
    parser.add_argument('-i', '--img', help="Url to image file to be enhanced.")
    
    parser.add_argument('--type', default='p', const='p', nargs='?', choices=['p','d','t'])
+   parser.add_argument('--method', default='va', const='va', nargs='?', choices=['v','va', 'k','b','d'])
  
    args = parser.parse_args()
    
    print(vars(args))
    
+   # Retrieve the coldef type.
    coldef_type = args.type
    print("The chosen coldef type is {}.".format(coldef_types[coldef_type]))
    
-   # Retrieve original image
+   # Retrieve the simulaiton method.
+   sim_method = args.method
+   print("The chosen simulation method is {}.".format(sim_methods[sim_method]))
    
+   # Retrieve original image.
    img = args.img
    if not img:
       print("No image has been chosen.")
@@ -56,10 +69,15 @@ def colordeficiency():
    elif not os.path.exists(img):
       print("The chosen image ({}) does not exist.".format(img))
    else:
-      pass
+      
+      # Show the original image.
       im_orig = Image.open(img)
       im_orig.show()
-      im_sim = simulate('vienot',im_orig,coldef_type)
+      
+      # Simulatte the original image.
+      im_sim = simulate(sim_methods[sim_method],im_orig,coldef_type)
+      
+      # Show the simulated image.
       im_sim.show()
 
 if __name__=='__main__':
