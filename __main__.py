50e06641776e55dfb9aabdad751afbcfa77b684b
Joschua Thomas Simon-Liedtke (win-10) <joschua@nr.no>
2023-03-20 16:28:05 +0100

    Reading image and simulating. -
    Enabled choosing of individual coldef_type.

--- __main__.py
+++ __main__.py
@@ -10,34 +10,57 @@
 
 '''
 
+#from os import path
+import sys
+#print(sys.executable)
+#sys.path.append(path.abspath('../colourspace'))
+#sys.path.append('..\colourspace')
+#print(path)
+import colour
 
 import argparse
 import os
 
 from PIL import Image
 
+
 from colordeficiency.colordeficiency import simulate
 
+coldef_types = {
+      'p': 'protan',
+      'd': 'deutan',
+      't': 'tritan'
+   }
 
 def colordeficiency():
    
    parser = argparse.ArgumentParser("A command line script to simulate color deficiency or daltonize images.")
  
    parser.add_argument('-i', '--img', help="Url to image file to be enhanced.")
+   
+   parser.add_argument('--type', default='p', const='p', nargs='?', choices=['p','d','t'])
  
    args = parser.parse_args()
+   
+   print(vars(args))
+   
+   coldef_type = args.type
+   print("The chosen coldef type is {}.".format(coldef_types[coldef_type]))
    
    # Retrieve original image
    
    img = args.img
    if not img:
       print("No image has been chosen.")
+      return
    elif not os.path.exists(img):
       print("The chosen image ({}) does not exist.".format(img))
    else:
-      
-      im = Image.open(img)
-      im.show()
+      pass
+      im_orig = Image.open(img)
+      im_orig.show()
+      im_sim = simulate('vienot',im_orig,coldef_type)
+      im_sim.show()
 
 if __name__=='__main__':
   colordeficiency()
